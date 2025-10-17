39f7848b789f93e69dc68c7615c4549df7906628
diibro <brother.dushime@gmail.com>
2025-10-17 15:08:46 +0200

    provider: adds global provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:my_journey/navigation/main_navigation.dart';
+import 'package:my_journey/providers/counter_provider.dart';
 import 'package:my_journey/theme/app_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => CounterProvider())
+        ],
+       child: const MainApp()
+      )
+    );
 }
 
 class MainApp extends StatelessWidget {
--- lib/navigation/main_navigation.dart
+++ lib/navigation/main_navigation.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_journey/providers/counter_provider.dart';
 import 'package:my_journey/screens/home_screen.dart';
 import 'package:my_journey/screens/profile_screen.dart';
 import 'package:my_journey/screens/settings_screen.dart';
+import 'package:provider/provider.dart';
 
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
@@ -13,7 +15,7 @@ class MainNavigation extends StatefulWidget {
 
 class _MainNavigationState extends State<MainNavigation> {
   int _currentIndex = 0;
-  int _count = 0;
+  // int _count = 0;
 
   final List<Widget> _screens = [
     HomeScreen(),
@@ -27,21 +29,23 @@ class _MainNavigationState extends State<MainNavigation> {
     });
   }
 
-  void _incrementCount(){
-    setState(() {
-      _count++;
-    });
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('FAB Counter clicked $_count times'), duration: const Duration(microseconds: 500),)
-    );
-  } 
+  // void _incrementCount(){
+  //   setState(() {
+  //     _count++;
+  //   });
+  //   ScaffoldMessenger.of(context).showSnackBar(
+  //     SnackBar(content: Text('FAB Counter clicked $_count times'), duration: const Duration(microseconds: 500),)
+  //   );
+  // } 
 
   @override
   Widget build(BuildContext context) {
+    final counterProvider = Provider.of<CounterProvider>(context);
+
     return Scaffold(
       body: _screens[_currentIndex],
       floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCount,
+          onPressed: counterProvider.increment,
           backgroundColor: Colors.deepPurple,
           child: const Icon(Icons.add, color: Colors.white, ),
         ),
@@ -51,9 +55,30 @@ class _MainNavigationState extends State<MainNavigation> {
         selectedItemColor: Colors.deepPurple,
         unselectedItemColor: Colors.grey,
         showUnselectedLabels: true,
-          items: const [
+          items: [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+            BottomNavigationBarItem(
+              icon: Stack(
+                children: [
+                  Icon(Icons.person),
+                  if(counterProvider.count > 0)
+                    Positioned(
+                        right: 0,
+                        top: 0,
+                        child: Container(
+                          padding: const EdgeInsets.all(3),
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle
+                          ),
+                          child: Text('${counterProvider.count}', style: TextStyle(color: Colors.white, fontSize: 10),),
+                        ),  
+                      )
+
+                ],
+              ), 
+              label: 'Profile'
+              ),
             BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
           ]
         ),
